92a2a8b749b5546a8c6d0386f4ab454c19eb4023
Jinkin <nadph513@outlook.com>
2024-10-01 18:22:04 +0000

    update: get posts from app apis

--- lib/config.ts
+++ lib/config.ts
@@ -339,8 +339,8 @@ export type Config = {
         thirdPartyApi?: string;
     };
     uestc: {
-        bbsCookie?: string;
-        bbsAuthStr?: string;
+        bbsToken?: string;
+        bbsSecret?: string;
     };
     weibo: {
         app_key?: string;
@@ -770,8 +770,8 @@ const calculateValue = () => {
             thirdPartyApi: envs.TWITTER_THIRD_PARTY_API,
         },
         uestc: {
-            bbsCookie: envs.UESTC_BBS_COOKIE,
-            bbsAuthStr: envs.UESTC_BBS_AUTH_STR,
+            bbsCookie: envs.UESTC_BBS_TOKEN,
+            bbsAuthStr: envs.UESTC_BBS_SECRET,
         },
         weibo: {
             app_key: envs.WEIBO_APP_KEY,
