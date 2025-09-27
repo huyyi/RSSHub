194730b12b740fd93d1cfa1031afa18ff7912527
Jinkin <nadph513@outlook.com>
2025-09-27 05:29:43 +0000

    fix: pubDate parse wrong and add all contents in description

--- lib/routes/uestc/bbs.ts
+++ lib/routes/uestc/bbs.ts
@@ -3,6 +3,7 @@ import ofetch from '@/utils/ofetch'; // 统一使用的请求库
 import { parseDate } from '@/utils/parse-date'; // 解析日期的工具函数
 import timezone from '@/utils/timezone';
 import { config } from '@/config';
+import cache from '@/utils/cache';
 
 export const route: Route = {
     path: '/bbs/:types?',
@@ -48,28 +49,79 @@ export const route: Route = {
     ],
     handler: async () => {
         const { bbs_token, bbs_secret } = config.uestc;
-        const authStr = bbs_token && bbs_secret ? `&accessToken=${bbs_token}&accessSecret=${bbs_secret}` : '';
-        const dataRaw = await ofetch(`https://bbs.uestc.edu.cn/mobcent/app/web/index.php?r=forum%2Ftopiclist&isImageList=1&page=1&pageSize=25&sortby=new${authStr}`);
+        const apiUrl = 'https://bbs.uestc.edu.cn/mobcent/app/web/index.php';
+        const topicListParams = {
+            r: 'forum/topiclist',
+            isImageList: 1,
+            page: 1,
+            pageSize: 25,
+            sortby: 'new',
+            accessToken: bbs_token,
+            accessSecret: bbs_secret,
+        };
+        const dataRaw = await ofetch(apiUrl, {
+            method: 'POST',
+            params: topicListParams,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            },
+        });
         const itemsRaw = dataRaw.list;
-        const items = itemsRaw.map((item) => ({
-            title: item.title,
-            link: `https://bbs.uestc.edu.cn/thread/${item.topic_id}`,
-            author: item.user_nick_name,
-            category: item.board_name,
-            img: item.pic_path,
-            pubDate: timezone(parseDate(item.last_reply_date), +8),
-            description: item.subject,
-            upvotes: item.hits,
-            comments: item.replies,
-        }));
+        const items = await Promise.all(
+            itemsRaw.map((item) =>
+                cache.tryGet(`uestc-bbs-${item.topic_id}`, async () => {
+                    const detailData = await ofetch(apiUrl, {
+                        method: 'POST',
+                        params: {
+                            r: 'forum/postlist',
+                            topicId: item.topic_id,
+                            page: 1,
+                            pageSize: 10,
+                            accessToken: bbs_token,
+                            accessSecret: bbs_secret,
+                        },
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                        },
+                    });
+                    // concat all contents
+                    const allContents = detailData.topic.content
+                        .map((post) => {
+                            switch (post.type) {
+                                // image
+                                case '1':
+                                    return `<img src="${post.infor}" alt="image"/>`;
+                                case '4':
+                                    return `<a href="${post.url}">${post.infor}</a>`;
+                                default:
+                                    return post.infor;
+                            }
+                        })
+                        .join('\n\n');
+                    return {
+                        title: item.title,
+                        link: `https://bbs.uestc.edu.cn/thread/${item.topic_id}`,
+                        author: item.user_nick_name,
+                        category: item.board_name,
+                        image: item.pic_path,
+                        description: allContents || item.subject,
+                        pubDate: timezone(parseDate(item.last_reply_date, 'x'), +8),
+                        upvotes: item.hits,
+                        comments: item.replies,
+                    };
+                })
+            )
+        );
         return {
             // 源标题
             title: '清水河畔',
             // 源链接
             link: 'https://bbs.uestc.edu.cn/new',
+            logo: 'https://bbs.uestc.edu.cn/assets/title-D_XPi883.ico',
+            icon: 'https://bbs.uestc.edu.cn/assets/title-D_XPi883.ico',
             // 源文章
             item: items,
-            language: 'zh',
+            language: 'zh-CN',
         };
     },
 };
